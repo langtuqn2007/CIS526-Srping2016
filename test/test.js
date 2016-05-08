f20cf6220db53f3d71a4ee86d98369e1f634b437
eujun <eujun32@gmail.com>
2016-05-08 02:28:38 -0500

    test case for to check only admin can delete hobbies now work

--- test/test.js
+++ test/test.js
@@ -6,7 +6,23 @@ var assert = require('assert');
 var test = require("../app");
 var supertest = require('supertest');
 var agent = supertest.agent(test);
+var server = supertest.agent('http://localhost:80/');
 
+
+function loginUser() {
+    return function(done) {
+        agent
+            .post('/login')
+            .send({ username: 'admin', password: 'insecurepassword' })
+            .expect(302)
+            .end(onResponse);
+
+        function onResponse(err, res) {
+           if (err) return done(err);
+           return done();
+        }
+    };
+};
 
 describe('GET /', function () {
   it('should return 200', function (done) {
@@ -36,7 +52,7 @@ describe('GET /logout', function () {
 });
 
 describe('POST /login', function() {
-  it('create a session logged in with admin username and password and should return 302', function(done){
+  it('should create a session logged in with admin username and password and should return 302', function(done){
     agent.post('/login')
     .send({username: 'admin', password: 'insecurepassword' })
     .end(function(err, res) {
@@ -47,7 +63,7 @@ describe('POST /login', function() {
 });
 
 describe('GET /hobby/:id/delete', function () {
-  it('Only admin and delete hobbies and should return 403', function (done) {
+  it('Only admin can delete hobbies therefore should return 403', function (done) {
     http.get('http://localhost:80/hobby/:id/delete', function (res) {
       assert.equal(403, res.statusCode);
       done();
@@ -55,21 +71,22 @@ describe('GET /hobby/:id/delete', function () {
   });
 });
 
-describe('GET /hobby/:id/delete', function () {
-  var j = request.jar();
-  var requestWithCookie = request.defaults({jar: j});
-
-  before(function(done) {
-    requestWithCookie.post('http://localhost:80/login', {username: 'admin', password: 'insecurepassword'},done());
-  });
-
-  it('should do something', function (done) {
-    requestWithCookie.get('http://localhost:80/hobby/:id/delete', function(err, res, user) {
-      assert.equal(200, res.statusCode);
-      done();
+describe('GET /hobby/:id/delete', function(){
+    it('should login with admin credentials and return 302', loginUser());
+    it('should successfully delete and return 302', function(done){
+    agent
+        .get('/hobby/:id/delete')
+        .expect(302)
+        .end(function(err, res){
+            if (err) return done(err);
+            done()
+        });
     });
-  });
 });
+
+
+
+
 
 describe('GET /invalidURL', function () {
   it('should return 404', function (done) {
